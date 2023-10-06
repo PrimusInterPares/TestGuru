4d9254b28eb1815e91ffad2f65efc10a5b6ee96c
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-06 14:42:03 +0300

    feature add `tests_by_category` class method to `Test` class

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+
+  # returns a descending array of names of all Tests whose Category is named in a certain way
+  def self.tests_by_category(category)
+    joins(:category)
+      .where(categories: { title: category })
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
