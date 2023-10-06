0406bc98e3bb3887657f5420948d0031be6c924e
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-06 14:32:19 +0300

    feature add model for `Passed_test` entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_130754) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_102503) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.float "value", null: false
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_130754) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passed_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.float "score"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
