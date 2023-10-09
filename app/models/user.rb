d9adbf5cf9174d04c624c902d67df8cfba23b35d
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-09 13:33:38 +0300

    feature add test_by_level instance method to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
+
+  # returns a list of all Tests that the User passes or has ever passed at this level of difficulty
+  def test_by_level(level)
+    Test.joins('JOIN tests_users ON tests.id = tests_users.test_id')
+        .where('tests_users.user_id = :id AND tests.level = :level', id:, level:)
+  end
 end
