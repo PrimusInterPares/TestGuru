af95d975c9b65bf253da99b0c3f345d14c691cdc
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-10 21:31:09 +0300

    feature add `Question` controller with `new`, `index`, `create`, `delete`, `show` methods. Add exception handler if a question was not created or deleted

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy]
+
+  def index
+    render inline: 'Questions within the test: <%= @test.questions.inspect %>'
+  end
+
+  def show
+    render inline: 'Question: <%= @question.inspect %>'
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      render plain: 'Question was created'
+    else
+      render plain: 'Question was not created'
+    end
+  end
+
+  def delete
+    if @question.destroy
+      render plain: 'Question was deleted'
+    else
+      render plain: 'Question was not deleted'
+    end
+  end
+
+  def new; end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
+end
