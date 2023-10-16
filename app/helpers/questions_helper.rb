a37fb0e4ed507e342c8af5fd4640e939249abf94
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-16 17:52:30 +0300

    style minor visual changes

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
   def question_header(test, question)
     if question.persisted?
-      "Edit question in \"#{question.test.title}\""
+      "Edit question in \"#{question.test.title}\" test"
     else
-      "Create new question into \"#{test.title}\""
+      "Create new question into \"#{test.title}\" test"
     end
   end
 end
