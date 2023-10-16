0e80966fd9c7979eade8b7a2b5512683825f6931
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-16 17:48:24 +0300

    feature add `Answers` into `routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'application#hello'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
