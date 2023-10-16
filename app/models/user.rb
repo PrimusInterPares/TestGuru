f388f044d2c6508f454100912ba5d24df4931f29
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-17 01:42:41 +0300

    feature change `test_by_level` method after `tests_users` table drop

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,12 @@ class User < ApplicationRecord
 
   # returns a list of all Tests that the User passes or has ever passed at this level of difficulty
   def test_by_level(level)
-    Test.joins(:tests_users)
-        .where('tests_users.user_id = :id AND tests.level = :level', id:, level:)
+    Test.joins(:test_passages)
+        .where('test_passages.user_id = :id AND tests.level = :level', id:, level:)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 
   private
