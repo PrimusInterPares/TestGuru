34f6e6d97718ca48564a0199e9ef8f3d178d80e6
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-27 20:36:25 +0300

    feat(session): add session controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to tests_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
