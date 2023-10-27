44b7687f017c886dd76eee8fb246c0c68434cc16
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-27 20:36:57 +0300

    feat(application): add application controller methods to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello! :)'
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    return if current_user
+
+    redirect_to login_path
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
   end
 end
