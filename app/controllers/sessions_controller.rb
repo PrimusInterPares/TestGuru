d98be38dd2fc452bd8d46dccb9af1633f8c7c4d8
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-30 15:18:59 +0300

    feat(session): add `destroy` method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,22 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
+    @_current_user = User.find_by(email: params[:email])
 
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @_current_user&.authenticate(params[:password])
+      session[:user_id] = @_current_user.id
       redirect_to tests_path, status: :see_other
     else
       flash.now[:alert] = 'Email or Password is incorrect. Please, enter your Email and Password.'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    # Remove the user id from the session
+    session.delete(:current_user_id)
+    # Clear the memoized current user
+    @_current_user = nil
+    redirect_to login_path, status: :see_other
   end
 end
