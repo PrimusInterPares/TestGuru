7716a76e5b79138c92beda8055c7c753a0ac1616
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-31 17:18:03 +0300

    feat: add mailer for TestPassage

--- app/controllers/test_passage_controller.rb
+++ app/controllers/test_passage_controller.rb
@@ -1,6 +1,4 @@
 class TestPassageController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound with: :rescue_with_test_pas_not_found
-
   before_action :set_test_passage, only: %i[result show update]
 
   def show; end
@@ -10,7 +8,9 @@ class TestPassageController < ApplicationController
   def update
     @test_passage.accept!(params[:answer_ids])
 
+
     if @test_passage.completed?
+      TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage), status: :see_other
     else
       render turbo_stream: turbo_stream.replace(
@@ -25,9 +25,5 @@ class TestPassageController < ApplicationController
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
-  end
-
-  def rescue_with_test_pas_not_found
-    render plain: 'Test passage was not found'
   end
 end
