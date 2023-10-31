f590981620ddd87541ee39c3d0b768c8d4bbf03b
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-31 21:51:30 +0300

    feat!: `Answer` resource management is now available only for `Admin`

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,7 @@
 class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  before_action :find_test, only: %i[index]
-
-  def index; end
-
   private
-
-  def find_test
-    @test = Test.find(params[:test_id])
-  end
 
   def rescue_with_question_not_found
     render plain: 'Question was not found'
