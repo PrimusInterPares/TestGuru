3ec10d569476380d421d255115d38eb479e75773
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-10-31 21:36:49 +0300

    feat: narrow application of `before_action :configure_sign_up_params` to devise controller `Users`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_sign_up_params, only: :create
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   before_action :authenticate_user!
+
+  protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name surname])
