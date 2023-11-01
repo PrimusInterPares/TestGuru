760b61b04e3f558854c8213a75555bcfeda301b2
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-11-01 12:58:08 +0300

    feat: change default locale to `:ru`

--- config/application.rb
+++ config/application.rb
@@ -9,16 +9,16 @@ Bundler.require(*Rails.groups)
 module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
-    config.active_support.cache_format_version = 7.0
+    config.active_support.cache_format_version = 7.1
 
     Rails.application.config.action_dispatch.cookies_serializer = :json
 
     config.time_zone = 'Moscow'
     config.active_record.default_timezone = :local
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
