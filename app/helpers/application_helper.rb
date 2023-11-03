95bc14dcbad8624687923fd5192e1043fefe1f5b
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-11-03 18:10:11 +0300

    feat: add styling using Bootstrap for `Tests` index, `navigation bars (for both user and admin`, `footer` add `Enter Admin Panel` button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
+  ALERTS = {
+    notice: 'info',
+    error: 'danger',
+    success: 'success',
+    alert: 'warning'
+  }.freeze
+
   def current_year
     Date.current.year
   end
 
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
+  end
+
+  def flash_alert(key)
+    ALERTS[key.to_sym] if flash[key]
   end
 end
