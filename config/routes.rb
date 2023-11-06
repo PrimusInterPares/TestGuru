7a386546cb028930e4a5fcd51f7968499758fdd4
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-11-06 18:30:18 +0300

    feat: add `Gist` model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    resources :gists, only: %i[index]
+  end
+
+  namespace :admin do
     resources :tests do
       resources :questions, shallow: true, except: :index do
         resources :answers, shallow: true, except: :index
