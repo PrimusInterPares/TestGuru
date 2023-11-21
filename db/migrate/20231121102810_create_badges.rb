fdb8d151a5bab1ef6557cdd703a52f1a9614cef9
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-11-21 18:41:54 +0300

    feat: add default for image_URL

--- db/migrate/20231121102810_create_badges.rb
+++ db/migrate/20231121102810_create_badges.rb
@@ -2,7 +2,7 @@ class CreateBadges < ActiveRecord::Migration[7.1]
   def up
     create_table :badges do |t|
       t.string :name, null: false, default: 'Badge'
-      t.string :image_url
+      t.string :image_url, default: 'https://cdn0.iconfinder.com/data/icons/education-science-2/100/Education__science_2_10-1024.png'
       t.string :rule
       t.string :parameter
 
