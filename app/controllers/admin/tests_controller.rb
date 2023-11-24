9804c4502eb829db33ea11c9d5faa3d0d08caffb
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-11-24 13:25:44 +0300

    feat: add `ready_to_be_run` field into views, add locales

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -50,7 +50,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :ready_to_be_run)
   end
 
   def rescue_with_test_not_found
