371fde2f03571ba6057cab10922c5ff3a7e9a6ec
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-12-03 17:42:07 +0300

    refactor: simplify validations for `Gist`

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,12 +1,10 @@
+# frozen_string_literal: true
+
 class Gist < ApplicationRecord
   belongs_to :question, class_name: 'Question', foreign_key: :question_id
   belongs_to :author, class_name: 'User', foreign_key: :author_id, optional: true
 
-  validates :author_id, presence: true
+  validates :author_id, :question_id, presence: true
 
-  validates :question_id, presence: true
-
-  validates :url, presence: true, uniqueness: true
-
-  validates :github_gist_id, presence: true, uniqueness: true
+  validates :url, :github_gist_id, presence: true, uniqueness: true
 end
