01292ed6b64c7d8f79cc87bd6871221364bae81f
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-12-10 14:16:26 +0300

    chore: add test for `TestMailer`

--- spec/mailers/tests_spec.rb
+++ spec/mailers/tests_spec.rb
@@ -1,5 +1,20 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe TestsMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'notifications' do
+    it 'notifies the user after test completion' do
+      user = create(:user)
+      test = create(:test)
+      test_passage = create(:test_passage, user:, test:)
+      email = TestsMailer.completed_test(test_passage)
+
+      assert_emails 1 do
+        email.deliver_now
+      end
+      assert_equal email.to, [user.email]
+      assert_equal email.from, ['mail@testguru.com']
+    end
+  end
 end
